29fd4a8a52112bc4a79364e1d9268009f0fb288d
Susan25 <susanmvogel@verizon.net>
2014-07-20 15:01:25 -0400

    Updated code to eliminate path dependencies

--- run_analysis.R
+++ run_analysis.R
@@ -1,29 +1,27 @@
-# Cleaning Data Projectt
+# Cleaning Data Project
 library(plyr)
 library(stringr)
 
 # Load in data sets - test & train and combine to form one dataset
 
-path = "C:/Users/Susan/Documents/Susan/DataScientistsToolbox/gettingcleaningdata/UCI HAR Dataset"
-
-trainx = read.table(paste0(path,"/train/X_train.txt"))
+trainx = read.table("./train/X_train.txt")
 
 #subset the data to only include our means and std devs
 
 trainx_subset = trainx[,c(1:6,41:46,81:86,121:126,161:166,201:202,214:215,227:228,240:241,253:254,266:271,
 345:350,424:429,503:504,516:517,529:530,542:543)]
 
-testx = read.table(paste0(path,"/test/X_test.txt"))
+testx = read.table("./test/X_test.txt")
 
 #subset out means and std devs - all we want to keep
 
 testx_subset = testx[,c(1:6,41:46,81:86,121:126,161:166,201:202,214:215,227:228,240:241,253:254,266:271,
                        345:350,424:429,503:504,516:517,529:530,542:543)]
 
-subject_train = read.table(paste0(path,"/train/subject_train.txt"))
+subject_train = read.table("./train/subject_train.txt")
 colnames(subject_train) = "Subject"
 
-subject_test = read.table(paste0(path,"/test/subject_test.txt"))
+subject_test = read.table("./test/subject_test.txt")
 colnames(subject_test) = "Subject"
 
 
@@ -42,8 +40,8 @@ combined_subject = rbind(subject_test,subject_train)
 
 # read in y values
 
-testy = read.table(paste0(path,"/test/y_test.txt"))
-trainy = read.table(paste0(path,"/train/y_train.txt"))
+testy = read.table("./test/y_test.txt")
+trainy = read.table("./train/y_train.txt")
 combinedy = rbind(testy,trainy)
 
 colnames(combinedy) = "Activity"
@@ -57,6 +55,10 @@ combinedy[combinedy==6] = "LAYING"
 
 combinedX = cbind(combined_subject, combinedy, combinedX)
 
+# get rid of big datasets (and others) that we no longer need
+
+#rm(testx,trainx,testx_subset,trainx_subset)
+
 # Order by Subject and then by Activity & calculate means of each column
 
 OrderedSummary = combinedX[order(combinedX$Subject,combinedX$Activity),]
@@ -67,4 +69,4 @@ nam <-names(OrderedSummary)
 names(OrderedSummary) <- ifelse(nam %in% c('Subject', 'Activity'), 
               str_c(nam, ''),  str_c('Avg ', nam))
 
-write.table(paste0(path,"/HumanActivityRecognitionTidy"))
+write.table("./HumanActivityRecognitionTidy")
